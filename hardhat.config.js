8a3a9fce03608b360ba2a8fe0bc40465711fbd25
Dhiraj Bari <98685837+dhi-raj07@users.noreply.github.com>
2023-06-25 14:06:22 +0530

    token contract and LP script

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,10 +11,4 @@ module.exports = {
       },
     }
   },
-  networks: {
-    goerli: {
-      url: "https://eth-goerli.g.alchemy.com/v2/XEL7Bu2in_J0nt2Lnpd3TW4O8TG7GLdj",
-      accounts: ["24d22ddbd5ee6c95e67cb20f94ee356a90bd1b1cdaf5b4d1025f055bd14ae566"]
-    }
-  }
 };
